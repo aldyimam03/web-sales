ac35aed4644260134a95c71062a858f055fd0dc0
Aldy Imam Wijaya <aldyimam03@gmail.com>
2025-09-20 18:07:48 +0700

    protect web route

--- routes/web.php
+++ routes/web.php
@@ -13,22 +13,25 @@ Route::get('login', [AuthController::class, 'loginPage'])->name('login');
 Route::post('login', [AuthController::class, 'login']);
 Route::post('logout', [AuthController::class, 'logout'])->name('logout');
 
-Route::prefix('items')->group(function () {
-    Route::get('/', [ItemController::class, 'index'])->name('items.index');
-    Route::get('/create', [ItemController::class, 'create'])->name('items.create');
-    Route::post('/', [ItemController::class, 'store'])->name('items.store');
-    Route::get('/{item}', [ItemController::class, 'show'])->name('items.show');
-    Route::get('/{item}/edit', [ItemController::class, 'edit'])->name('items.edit');
-    Route::put('/{item}', [ItemController::class, 'update'])->name('items.update');
-    Route::delete('/{item}', [ItemController::class, 'destroy'])->name('items.destroy');
-});
+Route::middleware('auth')->group(function () {
 
-Route::prefix('/users')->group(function () {
-    Route::get('/', [UserController::class, 'index'])->name('users.index');
-    Route::get('/create', [UserController::class, 'create'])->name('users.create');
-    Route::post('/', [UserController::class, 'store'])->name('users.store');
-    Route::get('/{user}', [UserController::class, 'show'])->name('users.show');
-    Route::get('/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
-    Route::put('/{user}', [UserController::class, 'update'])->name('users.update');
-    Route::delete('/{user}', [UserController::class, 'destroy'])->name('users.destroy');
+    Route::prefix('items')->group(function () {
+        Route::get('/', [ItemController::class, 'index'])->name('items.index');
+        Route::get('/create', [ItemController::class, 'create'])->name('items.create');
+        Route::post('/', [ItemController::class, 'store'])->name('items.store');
+        Route::get('/{item}', [ItemController::class, 'show'])->name('items.show');
+        Route::get('/{item}/edit', [ItemController::class, 'edit'])->name('items.edit');
+        Route::put('/{item}', [ItemController::class, 'update'])->name('items.update');
+        Route::delete('/{item}', [ItemController::class, 'destroy'])->name('items.destroy');
+    });
+
+    Route::prefix('/users')->group(function () {
+        Route::get('/', [UserController::class, 'index'])->name('users.index');
+        Route::get('/create', [UserController::class, 'create'])->name('users.create');
+        Route::post('/', [UserController::class, 'store'])->name('users.store');
+        Route::get('/{user}', [UserController::class, 'show'])->name('users.show');
+        Route::get('/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
+        Route::put('/{user}', [UserController::class, 'update'])->name('users.update');
+        Route::delete('/{user}', [UserController::class, 'destroy'])->name('users.destroy');
+    });
 });
