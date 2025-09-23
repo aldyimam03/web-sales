424d0b1b92060e0b3f3a91733b7c7b23cbe674fa
Aldy Imam Wijaya <aldyimam03@gmail.com>
2025-09-23 12:40:34 +0700

    fix ui

--- resources/views/payments/index.blade.php
+++ resources/views/payments/index.blade.php
@@ -33,7 +33,10 @@
             </div>
             <a href="{{ route('payments.create') }}"
                 class="inline-flex items-center px-6 py-3 bg-blue-600 text-white font-medium rounded-xl hover:bg-blue-700 shadow-md">
-                + Tambah Pembayaran
+                <svg class="w-5 h-5 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4v16m8-8H4"></path>
+                </svg>
+                Tambah Pembayaran
             </a>
         </div>
 
